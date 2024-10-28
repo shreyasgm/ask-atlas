4dba74bcffdb678b60c43f261b505dcffc576bad
Shreyas Gadgin Matha <shreyas.gm61@gmail.com>
2024-10-28 14:23:11 -0400

    Get some example queries in

---
+++ src/example_queries/unilateral_exports.sql
@@ -0,0 +1,36 @@
+-- This query answers the question: "What did the US export in 2022?"
+-- Assume that the user is asking about trade in both goods and services. We will assume HS 1992 classification for goods, and services_unilateral for services. We will use 1-digit product codes for both goods and services.
+
+-- Goods exports (HS92)
+SELECT 
+    'Goods' as category,
+    p.name_en as product_name,
+    p.code as product_code,
+    cpy.export_value,
+    cpy.global_market_share
+FROM hs92.country_product_year_1 cpy
+JOIN classification.location_country lc ON cpy.country_id = lc.country_id
+JOIN classification.product_hs92 p ON cpy.product_id = p.product_id
+WHERE cpy.year = 2022
+    AND cpy.export_value > 0
+    AND lc.iso3_code = 'USA'
+
+UNION ALL
+
+-- Services exports
+SELECT 
+    'Services' as category,
+    p.name_en as product_name,
+    p.code as product_code,
+    cpy.export_value,
+    cpy.global_market_share
+FROM services_unilateral.country_product_year_1 cpy
+JOIN classification.location_country lc ON cpy.country_id = lc.country_id
+JOIN classification.product_services_unilateral p ON cpy.product_id = p.product_id
+WHERE cpy.year = 2022
+    AND cpy.export_value > 0
+    AND lc.iso3_code = 'USA'
+
+ORDER BY 
+    category,
+    export_value DESC;
---
+++ src/example_queries/unilateral_exports_4_digit.sql
@@ -0,0 +1,15 @@
+-- This query answers the question: "What did the US export in 2022, at the 4-digit HS classification level?"
+SELECT 
+    p.code as hs_code,
+    p.name_en as product_name,
+    cpy.export_value,
+    cpy.global_market_share
+FROM hs92.country_product_year_4 cpy
+JOIN classification.location_country lc ON cpy.country_id = lc.country_id
+JOIN classification.product_hs92 p ON cpy.product_id = p.product_id
+WHERE cpy.year = 2022
+    AND cpy.export_value > 0
+    AND lc.iso3_code = 'USA'
+ORDER BY 
+    cpy.export_value DESC
+LIMIT 10;
